6f3d49d0c981f749093c13fe90da6ad6d5be8552
PaulGISer <paul.dolgi@yandex.ru>
2016-12-24 19:24:25 +0300

    Update bars.py

--- bars.py
+++ bars.py
@@ -1,21 +1,5 @@
 import json
-
-
-def load_data(filepath):
-    pass
-
-
-def get_biggest_bar(data):
-    pass
-
-
-def get_smallest_bar(data):
-    pass
-
-
-def get_closest_bar(data, longitude, latitude):
-    pass
-
-
-if __name__ == '__main__':
-    pass
+f=open('data.json','r',encoding='UTF-8')
+a=json.load(f)
+for i in a:
+    print (i['Name'])
